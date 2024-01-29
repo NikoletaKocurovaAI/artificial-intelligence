dbaa6ab00b2c0f0e5f3ea748ac6e6f92cee78d1b
Nikol <vladkototh@Vladkos-MacBook-Air.local>
2024-01-29 19:27:17 +0100

    refactor(ror/frontend-app): Refator html.

--- computer-vision/robot-object-recognition/src/python/frontend_app/frontend_app/settings.py
+++ computer-vision/robot-object-recognition/src/python/frontend_app/frontend_app/settings.py
@@ -16,6 +16,9 @@ from dotenv import load_dotenv
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# Set the STATIC_ROOT setting
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
