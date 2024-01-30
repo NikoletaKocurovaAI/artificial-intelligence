5e24e0bbd77b1220966dcdee3cf80de5bd901b46
Nikol <vladkototh@Vladkos-MacBook-Air.local>
2024-01-30 21:26:10 +0100

    refactor(ror/frontend-app): Add test.

---
+++ computer-vision/robot-object-recognition/src/python/frontend_app/dashboard/tests/test_e2e.py
@@ -0,0 +1,24 @@
+
+
+class EndToEndTestCase(TestCase):
+    def test_login_url_has_text(self):
+        selenium_webdriver = webdriver.Chrome()
+        selenium_webdriver.get("http://127.0.0.1:8000/")
+
+        assert "Login" in selenium_webdriver.page_source
+
+    def test_login_button_redirects_to_robot_run(self):
+        selenium_webdriver = webdriver.Chrome()
+        selenium_webdriver.get("http://127.0.0.1:8000/")
+
+        username_input = selenium_webdriver.find_element(By.ID, "username")
+        username_input.send_keys("admin")
+
+        password_input = selenium_webdriver.find_element(By.ID, "password")
+        password_input.send_keys("admin")
+
+        login_button = selenium_webdriver.find_element(By.ID, "login-button")
+        login_button.send_keys(Keys.RETURN)
+
+        assert "Robot run" in selenium_webdriver.page_source
+        
