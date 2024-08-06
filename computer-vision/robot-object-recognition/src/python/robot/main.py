feb1d6d3eae0a4e668e5e2139c0eff3e5f09579a
NikoletaKocurovaAI <nikoletakocurovaai@gmail.com>
2024-08-06 19:55:38 +0200

    refactor(ror/robot): Init read_ir_sensor_pulses as Thread

--- computer-vision/robot-object-recognition/src/python/robot/main.py
+++ computer-vision/robot-object-recognition/src/python/robot/main.py
@@ -1,5 +1,6 @@
 import RPi.GPIO as GPIO
 import time
+import threading
 # import cv2
 
 from constants import RpiPinsConstants as pins
@@ -14,21 +15,36 @@ GPIO.setmode(GPIO.BCM)
 # Set up motor 1 IR speed sensor
 GPIO.setup(pins.MOTOR1_IR_SPEED_SENSOR_PIN, GPIO.IN)
 
+motor1_rotations_counter = 0
+motor1_running = False
+
+
+def read_ir_sensor_pulses() -> None:
+    global motor1_rotations_counter, motor1_running
+
+    while motor1_running:
+        if GPIO.input(14):
+            motor1_rotations_counter += 1
+            print(f"IR sensor input {GPIO.input(14)}")
+
 
 def main():
+    global motor1_running
     # net, output_layers = object_detector.load_yolov3()
     # camera = cv2.VideoCapture(0)
 
     # running_robot_enabled: bool = True
     start_time: float = time.time()
-    rotations_counter: int = 0
 
+    ir_sensor = threading.Thread(target=read_ir_sensor_pulses)
+    ir_sensor.start()
+
+    motor1_running = True
     motor_controller.run("forwards")
 
     # while running_robot_enabled:
     while time.time() - start_time < 8:
-        if GPIO.input(14):
-            rotations_counter += 1
+        continue
 
         # print("reading frame")
         #
@@ -41,7 +57,11 @@ def main():
 
         # running_robot_enabled: bool = position_estimator.should_robot_continue()
 
-    print(f"No of rotations {rotations_counter}")
+    motor1_running = False
+    ir_sensor.join()
+
+    print(f"No of rotations {motor1_rotations_counter}")
+
     motor_controller.stop()
 
     # Release camera and close windows
